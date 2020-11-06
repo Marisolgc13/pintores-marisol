b50ce527db9f1a365afccde07d78426c98a09f24
Marisolgc13 <64939683+Marisolgc13@users.noreply.github.com>
2020-11-05 18:07:13 -0700

    Create footer.blade.php

---
+++ incluye/footer.blade.php
@@ -0,0 +1,23 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<body>
+    <footer style="background-color:#A83535">
+<div class="Informacion">  
+    <a href="#"></a>Marisol Gómez Chacón
+    <br>
+    <a href="#"></a>Programacion
+    <br>
+    <a href="#"></a>CBTis 12
+   <br>
+    <a href="#"></a>Practica Pintores
+
+
+</footer>
+</div>
+</body>
+</html>
