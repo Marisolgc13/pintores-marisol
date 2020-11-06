34607116d9cf0361a3f37c6ef125e6988fd61c17
Marisolgc13 <64939683+Marisolgc13@users.noreply.github.com>
2020-11-05 18:11:21 -0700

    Create plantilla.blade.php

---
+++ plantilla.blade.php
@@ -0,0 +1,9 @@
+@extends('layouts.master')
+@section('title','plantilla')
+@section('header')
+<center><h1>Marisol Gómez Chacón</h1></center>
+<center><img src="images/unnamed.png" alt=""></center>
+@stop
+@section('navbar')
+@parent 
+@stop
