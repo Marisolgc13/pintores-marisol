da425ad9b8d81b0412c818e093632a00bfb343d8
Marisolgc13 <64939683+Marisolgc13@users.noreply.github.com>
2020-11-05 18:09:51 -0700

    Create dali.blade.php

---
+++ dali.blade.php
@@ -0,0 +1,40 @@
+@extends('layouts.master')
+@section('title','Dali')
+@section('header')
+
+@stop
+@section('navbar')
+@show
+@section('principal')
+<h1>Salvador Dali</h1>
+    <h2>Biografia</h2>
+    <br>
+    <h3> Nació un 11 de mayo de 1904 en el número 20 de la calle Monturiol de Figueras (Gerona; España).
+        Se adentraría en el mundo de la pintura con tan solo 12 años de la mano de unos amigos de sus padres, los Pichot. Gracias a esta familia de artistas y especialmente a la figura de Ramón Pichot, Dalí descubrió el impresionismo.
+        A los 18 se trasladó a la Residencia de Estudiantes de Madrid para estudiar en la Real Academia de Bellas Artes de San Fernando. Allí conoció y estableció fuertes lazos con algunas futuras celebridades del arte español como Federico García Lorca o Luis Buñuel. Uno de los hechos más curiosos y que más se recuerdan de esta época fue su segunda y definitiva expulsión de la Academia en 1926. El propio artista contó en una entrevista que, un día de examen, en una prueba oral de historia del arte para ser más exactos, se negó a hablar del artista Rafael en frente del tribunal que lo examba, alegando que los evaluadores presentes no estaban capacitados para examinarle, puesto que él había leído y sabía mucho más que ellos tres juntos sobre el famoso renacentista italiano.
+        Un año más tarde, en 1927, viajó a París, donde conocería a uno de las pocas personas que llegó idolatrar en su vida: Pablo Picasso. Esa ciudad, además, vería florecer el surrealismo de Dalí así como ese característico bigote inspirado en Velázquez que le acompañaría el resto de su vida.
+        En 1929 conoció a la que sería su gran amor y musa, una inmigrante rusa 11 años mayor que él conocida con el nombre de Gala.
+        En 1931 pintaría uno de sus cuadros más famosos: "la persistencia de la memoria" (también conocido como "los relojes blandos") inspirado, según el propio autor, en trozos de queso Cambembert derritiéndose al sol.
+        En 1941 él y Gala se mudaron a Estados Unidos. Allí pasaron 8 años hasta que en 1949 decidieran a Cataluña, tierra donde permanecieron hasta sus últimos días. Este último período de su vida y su trabajo artístico estuvo especialmente relacionado con la ciencia [* Si os interesa, este documental explica la influencia de la ciencia en sus obras], la religión y otras actividades creativas (creó el logo actual de la compañía Chupa Chups, realizó un anuncio televisivo para la marca de chocolate Lanvin y participó en la campaña publicitaria de Eurovisión, ... entre otras). Finalmente fallecería el 23 de enero de 1989 por una parada cardiorrespiratoria a los 84 años de edad. </ h3 >
+        <br>
+        <h2>Pinturas</h2>
+        <br>
+        <h3>Cristo de San Juan de la cruz</h3>
+        <img src="images/cristo.jpg" alt="">
+        <br>
+        <h3>La persistencia de la memoria</h3>
+        <img src="images/pesistencia.jpg" alt="">
+        <br>
+        <h3>Sueño producido por el vuelo de una abeja alrededor de una granada un segundo antes de despertar</h3>
+        <img src="images/sueño.jpg" alt="">
+        <br>
+        <h3>Construccion blanda con judias hervidas</h3>
+        <img src="images/construccion.jpg" alt="">
+        <br>
+        <h3>La metamorfosis de Narciso</h3>
+        <img src="images/metamorfosis.jpg" alt="">
+        @stop
+    @section('footer')
+        <img src="" alt="">
+    @parent 
+   @stop
