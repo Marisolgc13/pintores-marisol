f8749fcd9eb9ac162744e6acf02195201d180c02
Marisolgc13 <64939683+Marisolgc13@users.noreply.github.com>
2020-11-05 18:08:09 -0700

    Create header.blade.php

---
+++ incluye/header.blade.php
@@ -0,0 +1,22 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<body>
+    
+</body>
+</html>
+    <title>@yield('title')</title>
+</head>
+<body style="background-color: white">
+@section('header')
+<header style="background-color:#A83535">
+<div class="titulo"></div>
+<h1>Pintores</h1>
+</header>
+@show
+  </html>  
+</body>
